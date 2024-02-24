e0765b84dc0f8be16531d989ba9d7f9fb1037491
Oscar Sevilla <isc.ossevgar@me.com>
2024-02-24 09:19:59 -0600

    Init Project

---
+++ Prueba Tecnica iOS/Prueba Tecnica iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Prueba Tecnica iOS
+//
+//  Created by Etwan on 24/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
